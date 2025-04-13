b33a5a57891e3309d3596af40f4b3af0773f6b46
Yash Gupta <writingnaabbit@gmail.com>
2025-04-13 21:46:55 +0530

    changes done and Images Added on Page 1

--- kandivaliPhoolMarket/kandivaliPhoolMarket/settings.py
+++ kandivaliPhoolMarket/kandivaliPhoolMarket/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
