939d53b54dbb7c95e0cf40f7149afc8e0edf2fdf
Yash Gupta <writingnaabbit@gmail.com>
2025-05-11 21:42:12 +0530

    Update settings.py

--- kandivaliPhoolMarket/kandivaliPhoolMarket/settings.py
+++ kandivaliPhoolMarket/kandivaliPhoolMarket/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-j(jng#ucosv2)4bb*pmh-*bl!e^0-0nn2!7%*rq)7j-r2&pu&l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["107.21.154.93",
+ALLOWED_HOSTS = ["54.242.20.192",
                  "127.0.0.1",
                  "www.kandivaliphoolmarket.com",
                  ]
